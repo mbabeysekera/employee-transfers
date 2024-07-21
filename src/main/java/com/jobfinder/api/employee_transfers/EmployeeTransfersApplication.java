b22de164f4fa8e076989dd8e847abd31ab32acd2
babeysekera <revolutiongc.lk@gmail.com>
2024-07-21 11:58:01 +0530

    initial commit

---
+++ src/main/java/com/jobfinder/api/employee_transfers/EmployeeTransfersApplication.java
@@ -0,0 +1,13 @@
+package com.jobfinder.api.employee_transfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTransfersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTransfersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobfinder/api/employee_transfers/EmployeeTransfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobfinder.api.employee_transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTransfersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
